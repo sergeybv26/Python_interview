64cabec593b56c4bfa73f309bd847007dfb4a30a
ssukhanov <sergey.bv26@gmail.com>
2021-04-24 14:11:53 +0300

    Начата работа над 5 ДЗ, но вообще не понятно как его делать

--- Lesson4/shop_prj/shop_prj/settings.py
+++ Lesson4/shop_prj/shop_prj/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop_app.apps.ShopAppConfig',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- Lesson4/shop_prj/shop_prj/urls.py
+++ Lesson4/shop_prj/shop_prj/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop_app.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
